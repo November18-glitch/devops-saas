c3b96be0da7e93e8ec30fe3b810d62e9f23fd5be
November18-glitch <alyseaschneck@aol.com>
2026-03-21 14:55:38 +0100

    update

--- frontend/api/deployProject.js
+++ frontend/api/deployProject.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+  console.log("RAW repoUrl:", repoUrl)
   try {
     if (req.method !== "POST") {
       return res.status(405).json({ error: "Method not allowed" });
