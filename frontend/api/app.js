5b95c494fa45b1a079d8bcf08cb3b16ca52110ee
November18-glitch <alyseaschneck@aol.com>
2026-05-25 11:43:41 +0200

    fix app import

--- frontend/api/app.js
+++ frontend/api/app.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import analyzeRepo from "./analyzeRepo";
+import analyzeRepo from "./analyzeRepo.js";
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
