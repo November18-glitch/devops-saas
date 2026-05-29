8242b961043323a43ae50d3a4822c2f821d99713
November18-glitch <alyseaschneck@aol.com>
2026-05-29 16:25:53 +0200

    update

--- frontend/src/lib/supabase.js
+++ frontend/src/lib/supabase.js
@@ -1,6 +1,19 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+console.log(
+  "SUPABASE URL:",
+  import.meta.env.VITE_SUPABASE_URL
+);
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+console.log(
+  "SUPABASE KEY:",
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
+
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
+
+export default supabase;
